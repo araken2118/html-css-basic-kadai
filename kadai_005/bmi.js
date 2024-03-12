c8c134832af4c23721811e6150058d9d0c778a30
araken2118 <72079619+araken2118@users.noreply.github.com>
2024-03-12 09:12:23 +0900

    Delete kadai_005/bmi.js

--- kadai_005/bmi.js
+++
@@ -1,9 +0,0 @@
-//変数の宣言・値の代入
-let userWeight = 68;
-let userHight = 1.7;
-let userBMI = userWeight/(userHight*userHight);
-
-//コンソールへの出力
-console.log(userWeight);
-console.log(userHight);
-console.log(userBMI);
